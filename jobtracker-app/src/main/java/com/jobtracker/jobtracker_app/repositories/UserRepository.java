4ede5167cbb0101ff850daaecfdfeba17181efc4
Nam <ptnam1672003@gmail.com>
2025-11-28 22:35:02 +0700

    fix(search-user): fix n+1 query

--- jobtracker-app/src/main/java/com/jobtracker/jobtracker_app/repositories/UserRepository.java
+++ jobtracker-app/src/main/java/com/jobtracker/jobtracker_app/repositories/UserRepository.java
@@ -12,18 +12,33 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends JpaRepository<User, String> {
-    @Query(value = "SELECT * FROM users u " +
-            "WHERE u.deleted_at IS NULL " +  // soft delete
+    // N+1 QUERY
+//    @Query(value = "SELECT * FROM users u " +
+//            "WHERE u.deleted_at IS NULL " +
+//            "AND (:keyword IS NULL OR " +
+//            "u.email LIKE CONCAT('%', :keyword, '%') OR " +
+//            "u.first_name LIKE CONCAT('%', :keyword, '%') OR " +
+//            "u.last_name LIKE CONCAT('%', :keyword, '%') OR " +
+//            "u.phone LIKE CONCAT('%', :keyword, '%')" +
+//            ") " +
+//            "AND (:roleId IS NULL OR u.role_id = :roleId) " +
+//            "AND (:isActive IS NULL OR u.is_active = :isActive) " +
+//            "AND (:emailVerified IS NULL OR u.email_verified = :emailVerified) " +
+//            "ORDER BY u.created_at DESC", nativeQuery = true)
+
+    @Query(value = "SELECT u FROM User u " +
+            "JOIN FETCH u.role r " +
+            "WHERE u.deletedAt IS NULL " +
             "AND (:keyword IS NULL OR " +
             "u.email LIKE CONCAT('%', :keyword, '%') OR " +
-            "u.first_name LIKE CONCAT('%', :keyword, '%') OR " +
-            "u.last_name LIKE CONCAT('%', :keyword, '%') OR " +
+            "u.firstName LIKE CONCAT('%', :keyword, '%') OR " +
+            "u.lastName LIKE CONCAT('%', :keyword, '%') OR " +
             "u.phone LIKE CONCAT('%', :keyword, '%')" +
             ") " +
-            "AND (:roleId IS NULL OR u.role_id = :roleId) " +
-            "AND (:isActive IS NULL OR u.is_active = :isActive) " +
-            "AND (:emailVerified IS NULL OR u.email_verified = :emailVerified) " +
-            "ORDER BY u.created_at DESC", nativeQuery = true)
+            "AND (:roleId IS NULL OR r.id = :roleId) " +
+            "AND (:isActive IS NULL OR u.isActive = :isActive) " +
+            "AND (:emailVerified IS NULL OR u.emailVerified = :emailVerified) " +
+            "ORDER BY u.createdAt DESC")
     Page<User> findAllAndSearch(
             @Param("keyword") String keyword,
             @Param("roleId") String roleId,
