aab0baee4248e8e058b82023e0c1466f1dacb0c6
Nam <ptnam1672003@gmail.com>
2026-02-02 22:52:24 +0700

    feat(job-resume): add job resume entity and repository

---
+++ jobtracker-app/src/main/java/com/jobtracker/jobtracker_app/repositories/JobResumeRepository.java
@@ -0,0 +1,11 @@
+package com.jobtracker.jobtracker_app.repositories;
+
+import com.jobtracker.jobtracker_app.entities.JobResume;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JobResumeRepository extends JpaRepository<JobResume, String> {
+}
+
+
+
+
