e39c36c4ce599acd06541b909ddf78db661c0724
Nam <ptnam1672003@gmail.com>
2026-02-02 22:50:36 +0700

    feat(job-skill): add job skill repository

---
+++ jobtracker-app/src/main/java/com/jobtracker/jobtracker_app/repositories/JobSkillRepository.java
@@ -0,0 +1,23 @@
+package com.jobtracker.jobtracker_app.repositories;
+
+import com.jobtracker.jobtracker_app.entities.JobSkill;
+import com.jobtracker.jobtracker_app.entities.Skill;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface JobSkillRepository extends JpaRepository<JobSkill, String> {
+    // SELECT * FROM job_skill js JOIN skill s ON js.skill_id = s.id ...
+    @Query("SELECT js FROM job_skills js " +
+            "JOIN FETCH js.skill s " +
+            "WHERE js.job.id =: jobId " +
+            "AND js.isDelete = false " +
+            "ORDER BY s.name ASC"
+    )
+    List<JobSkill> findByJobIdWithSkill(@Param("jobId") String jobId);
+}
+
+
+
